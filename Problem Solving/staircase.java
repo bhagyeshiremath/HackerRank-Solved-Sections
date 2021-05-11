9f78a98588c1906539e994cffd613634fecf7736
Bhagyesh Hiremath <65004269+bhagyeshiremath@users.noreply.github.com>
2021-05-11 11:58:51 +0530

    Create staircase.java

---
+++ Problem Solving/staircase.java
@@ -0,0 +1,32 @@
+public static void staircase(int n) 
+    {
+        /*for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j < (n-1-i))
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print("#");
+                }
+            }
+            System.out.println("");
+        }*/
+        
+        
+        for(int I=0; I<n;I++)
+        {
+          for(int j=0; j<(n-I-1);j++)
+          {
+            System.out.print(" ");
+          }
+          for(int k=0;k<=I;k++)
+          {
+            System.out.print("#");
+          }
+          System.out.println("");
+          }
+        }
