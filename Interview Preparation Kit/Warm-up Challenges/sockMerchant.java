ddcb6152a852d421d972ee4307cf9d811017f5bd
Bhagyesh Hiremath <65004269+bhagyeshiremath@users.noreply.github.com>
2021-05-11 12:40:34 +0530

    Update sockMerchant.java

--- Interview Preparation Kit/Warm-up Challenges/sockMerchant.java
+++ Interview Preparation Kit/Warm-up Challenges/sockMerchant.java
@@ -1,1 +1,33 @@
 
+public static int sockMerchant(int n, List<Integer> ar) 
+    {
+       /* ArrayList<Integer> store = new ArrayList<Integer>();
+        int k = 0;
+        for(int i = 0 ; i < n; i ++)
+        {
+            int element = ar.get(i);
+            if(store.contains(element))
+            {
+                store.remove(element);
+                k++;
+            }
+            else
+            {
+                store.add(element);
+            }
+        }
+        return k;*/
+        ArrayList <Integer> store= new ArrayList<Integer>();
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(!store.contains(ar.get(i)))
+               store.add(ar.get(i));
+        }
+        int cnt=0;
+        for(int i = 0 ; i < store.size() ; i++)
+        {
+            cnt = cnt + Collections.frequency(ar,store.get(i))/2;
+        }
+        return cnt;
+    }
+}
